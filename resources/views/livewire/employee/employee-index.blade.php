6b29d611519744b0af8d2860255a87a6478327f7
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-12-13 10:44:01 -0300

    fix: htacess and employee index fixing

--- resources/views/livewire/employee/employee-index.blade.php
+++ resources/views/livewire/employee/employee-index.blade.php
@@ -45,35 +45,43 @@
         </thead>
 
         <tbody class="dark:bg-gray-700 dark:text-white text-black bg-white">
-        @foreach ($employees as $employee)
+        @if(count($employees) > 0)
+            @foreach ($employees as $employee)
+                <tr>
+                    <td class="p-3 font-medium text-center flex items-center justify-center gap-4">
+                        <a href="{{ route('employee.show', ['id' => $employee->getId()]) }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            </svg>
+                        </a>
+
+
+                        {{ $employee->getName() }}
+                    </td>
+
+                    <td class="p-3 font-medium text-center hidden sm:table-cell">
+                        {{ $employee->getCpf() }}
+                    </td>
+
+                    <td class="p-3 font-medium text-center hidden sm:table-cell">
+                        {{ $employee->getPhone() }}
+                    </td>
+
+                    <td class="p-3 font-medium text-center hidden sm:table-cell">
+                        {{ $employee->getEmail() }}
+                    </td>
+                </tr>
+            @endforeach
+        @else
             <tr>
-                <td class="p-3 font-medium text-center flex items-center justify-center gap-4">
-                    <a href="{{ route('employee.show', ['id' => $employee->getId()]) }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-                        </svg>
-                    </a>
-
-
-                    {{ $employee->getName() }}
-                </td>
-
-                <td class="p-3 font-medium text-center hidden sm:table-cell">
-                    {{ $employee->getCpf() }}
-                </td>
-
-                <td class="p-3 font-medium text-center hidden sm:table-cell">
-                    {{ $employee->getPhone() }}
-                </td>
-
-                <td class="p-3 font-medium text-center hidden sm:table-cell">
-                    {{ $employee->getEmail() }}
+                <td colspan="4" class="p-3 text-center">
+                    {{ __('No employees') . '...' }}
                 </td>
             </tr>
-        @endforeach
+        @endif
         </tbody>
     </table>
 
